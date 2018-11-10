fe8e2b5db427057fe4e198340bb7c5bfa8c7796b
Lele <maggiraffaele@gmail.com>
2018-11-10 22:52:24 +0100

    messe a poste le classi

--- Server_msc/WindowsFormsApp1/ISII.cs
+++ Server_msc/WindowsFormsApp1/ISII.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApp1
-{
-    class ISII
-    {
+namespace WindowsFormsApp1 {
+    class ISII {
         //Dichiarazione
         public static string[] DB_InsegnantiISII = new string[] { };
         public static string[] DB_AllieviISII = new string[] { };
 
         //Inizializzazione
-        public ISII()
-        {
+        public ISII() {
             Array.Resize(ref DB_InsegnantiISII, DB_InsegnantiISII.Length + 3);
             DB_InsegnantiISII[0] = " Mario Rossi 3339065819";
             DB_InsegnantiISII[1] = " Davide Verdi 3349069719";
@@ -25,7 +22,18 @@ namespace WindowsFormsApp1
             DB_AllieviISII[1] = " Gandolfo Blu 3324966819";
             DB_AllieviISII[2] = " Elia Bianchi 3336368889";
         }
-       
-        //aa
+
+        public void ISII_Insegnante(string nome, string cognome, string numero) {
+            Array.Resize(ref DB_InsegnantiISII, DB_InsegnantiISII.Length + 1);
+            DB_InsegnantiISII[DB_InsegnantiISII.Length - 1] = nome + " " + cognome + " " + numero;
+
+        }
+
+        public void ISII_Allievo(string nome, string cognome, string numero) {
+            Array.Resize(ref DB_InsegnantiISII, DB_InsegnantiISII.Length + 1);
+            DB_InsegnantiISII[DB_InsegnantiISII.Length - 1] = nome + " " + cognome + " " + numero;
+
+        }
+
     }
 }
